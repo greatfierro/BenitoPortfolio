1c3894c062e4f6729cb0808295a85b9ab114ec13
Benito <contact@benitopatino.com>
2020-06-21 20:16:31 -0700

    Added captcha div

---
+++ public/app.js
@@ -0,0 +1,35 @@
+// Getting elements
+const form = document.getElementById("contact-form");
+
+// Adding event listener
+form.addEventListener("submit", submitForm);
+
+// Submit Form
+function submitForm(e) {
+  // get all form input values
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const subject = document.querySelector("#subject").value;
+  const message = document.querySelector("#message").value;
+  const captcha = document.querySelector("#g-recaptcha-response").value;
+  fetch("/submit", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      subject,
+      message,
+      captcha,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      alert(data.msg);
+    });
+  e.preventDefault();
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-const Mail = require("nodemailer/lib/mailer");
+const fetch = require("node-fetch");
 const app = express();
 
 const PORT = 5000;
@@ -9,8 +9,39 @@ const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/send", (req, res) => {
-  // Create Transporter
+// Middleware
+app.use(express.static("public"));
+
+// Home Page
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+// handle form submit
+app.post("/submit", async (req, res) => {
+  if (!req.body.captcha) {
+    return res.json({ success: false, msg: "Please select captcha" });
+  }
+
+  // verufy url string
+  const secretKey = process.env.SECRET_KEY;
+  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+
+  // //Make request to VerifyUrl
+  const body = await fetch(verifyUrl).then((res) => res.json());
+
+  // If not successful
+  if (body.success !== undefined && !body.success)
+    return res.json({ success: false, msg: "Failed captcha verification" });
+
+  // If successful
+  sendEmail(req.body.name, req.body.email, req.body.subject, req.body.message);
+  return res.json({ success: true, msg: "Captcha passed" });
+});
+
+// use Nodemail to send email
+function sendEmail(name, email, subject, message) {
+  // Create Transporterß
   const transporter = nodemailer.createTransport({
     host: process.env.MAILHOST,
     port: process.env.MAILPORT,
@@ -26,19 +57,19 @@ app.post("/send", (req, res) => {
 
   // Send Data
   let info = transporter.sendMail({
-    from: req.body.email, // sender address
+    from: email, // sender address
     to: process.env.MAILUSER, // list of receivers
-    subject: req.body.subject, // Subject line
-    text: req.body.message, // plain text body
-    html: `<p>${req.body.message}</p>`, // html body
+    subject: subject, // Subject line
+    text: message, // plain text body
+    html: `<p>${message}</p>`, // html body
   });
-  console.log(req.body.name);
-  console.log(req.body.email);
-  console.log(req.body.subject);
-  console.log(req.body.message);
-  res.redirect("http://localhost:5501/");
-});
+}
 
+// Return an objct with the following properties
+// Success : true/false
+function validateCaptcha(captcha, remoteip) {}
+
+// test route
 app.get("/env", (req, res) => {
   console.log(typeof process.env.MAILHOST);
 });
